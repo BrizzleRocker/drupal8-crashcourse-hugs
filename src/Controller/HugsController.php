551d05bd6f6a18e3abb901a830fb4f99c24a547a
Andrew Dempster <andrew@microserveltd.co.uk>
2015-08-18 15:27:58 +0100

    Tutorial Final - got the node thingy working
    Added the node modifier - suggest this gets updated to use a template

--- src/Controller/HugsController.php
+++ src/Controller/HugsController.php
@@ -5,6 +5,7 @@ namespace Drupal\hugs\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\hugs\HugTracker;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\node\NodeInterface;
 
 class HugsController extends ControllerBase {
   protected $hugTracker;
@@ -35,4 +36,30 @@ class HugsController extends ControllerBase {
     return $return;
   }
 
+  public function nodeHug(NodeInterface $node) {
+    if ($node->isPublished()) {
+      // These are the same!
+      //$body = $node->body->value;
+      //$body = $node->body[0]->value;
+      // But we really want...
+      $formatted = $node->body->processed;
+
+      $terms = [];
+      foreach ($node->field_tags as $tag) {
+        $terms[] = $tag->entity->label();
+      }
+
+      $message = $this->t('Everyone hug @name because @reasons!', [
+        '@name' => $node->getOwner()->label(),
+        '@reasons' => implode(', ', $terms),
+      ]);
+      
+      // this should be in a template file!
+      return [
+        '#title' => $node->label() . ' (' . $node->bundle() . ')',
+        '#markup' => $message . $formatted,
+      ];
+    }
+    return $this->t('Not published');
+  }
 }
